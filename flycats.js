dc433c25d297c0b21e35596ec801069e5c2ebe72
Jon Landa <jonlanda06@gmail.com>
2022-03-28 14:09:30 +0200

    Made function for flying cats

--- flycats.js
+++ flycats.js
@@ -1,8 +1,14 @@
 function fly() {
     let x = Math.floor((Math.random() * 2) + 1);
-
+    console.log(x);
     if (x == 1) {
+        console.log(x);
         let cat1 = document.getElementById("flycat1");
         cat1.style.display = "initial";
+        cat1.style.animation = "catmove 15s";
+    } else {
+        let cat2 = document.getElementById("flycat2");
+        cat2.style.display = "initial";
+        cat2.style.animation = "catmove 15s";
     }
 }
